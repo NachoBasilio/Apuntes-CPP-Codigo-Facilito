7f3cc5416000409b333c977bdd3022af381d14b4
IgnaDev <arcortoon@gmail.com>
2023-05-03 19:19:44 -0300

    algunos tipos

--- codigoFacilito.cpp
+++ codigoFacilito.cpp
@@ -2,6 +2,13 @@
 using namespace std;
 
 int main(){
+    int numero = 0;
+    float numeroFlotante = 0.0;
+    double numeroDoble = 0.0;
+    char caracter = 'a';
+    bool booleano = true;
+    string cadena = "Hola mundo";
     cout << "Hola mundo!" << endl;
     return 0;
 }
+
