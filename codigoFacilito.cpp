32a42ef496e941c2eb495b1f69330b7f2d62f15c
IgnaDev <arcortoon@gmail.com>
2023-05-05 17:34:22 -0300

    por ver los ultimos temas

--- codigoFacilito.cpp
+++ codigoFacilito.cpp
@@ -2,12 +2,6 @@
 using namespace std;
 
 int main(){
-    int numero = 0;
-    float numeroFlotante = 0.0;
-    double numeroDoble = 0.0;
-    char caracter = 'a';
-    bool booleano = true;
-    string cadena = "Hola mundo";
     cout << "Hola mundo!" << endl;
     return 0;
 }
