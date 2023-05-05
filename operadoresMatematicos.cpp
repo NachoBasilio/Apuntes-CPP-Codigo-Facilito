43d9185d26749cde0c37f15f1f31ec7feef7a10a
IgnaDev <arcortoon@gmail.com>
2023-05-05 19:12:54 -0300

    operadores matematicos

---
+++ operadoresMatematicos.cpp
@@ -0,0 +1,20 @@
+//crea yb codico con los principales operadores matemacitos en cpp
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    int a = 5;
+    int b = 2;
+    int c = a + b;
+    int d = a - b;
+    int e = a * b;
+    int f = a / b;
+    int g = a % b;
+    cout << "a + b = " << c << endl;
+    cout << "a - b = " << d << endl;
+    cout << "a * b = " << e << endl;
+    cout << "a / b = " << f << endl;
+    cout << "a % b = " << g << endl;
+    return 0;
+}
