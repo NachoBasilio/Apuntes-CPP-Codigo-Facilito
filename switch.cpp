aa87a50a1cbfaba4ae20b6f58348b27f1208107e
IgnaDev <arcortoon@gmail.com>
2023-05-05 19:52:48 -0300

    switch

--- switch.cpp
+++ switch.cpp
@@ -8,19 +8,19 @@ int main(){
     switch (a)
     {
     case 1:
-        cout << "El numero es 1" << endl;
+        cout << "El numero es 1!" << endl;
         break; 
     case 2: 
-        cout << "El numero es 2" << endl;
+        cout << "El numero es 2!" << endl;
         break;
     case 3:
-        cout << "El numero es 3" << endl;
+        cout << "El numero es 3!" << endl;
         break;
     case 4:
-        cout << "El numero es 4" << endl;
+        cout << "El numero es 4!" << endl;
         break;
     case 5:
-        cout << "El numero es 5" << endl;
+        cout << "El numero es 5!" << endl;
         break;
     default:
         cout << "El numero no esta en el rango" << endl;
